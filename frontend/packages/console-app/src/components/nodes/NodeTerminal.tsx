c49ab10e8b4e434ee2b89b031bbc794c4c53f8a9
chris93111 <christopheferreira@ymail.com>
2022-04-10 21:40:43 +0200

    standard terminal

--- frontend/packages/console-app/src/components/nodes/NodeTerminal.tsx
+++ frontend/packages/console-app/src/components/nodes/NodeTerminal.tsx
@@ -29,7 +29,7 @@ const getDebugImage = async (): Promise<string> => {
     const istag = await k8sGet(ImageStreamTagModel, 'tools:latest', 'openshift');
     return istag.image.dockerImageReference;
   } catch (e) {
-    return 'registry.redhat.io/rhel8/support-tools';
+    return 'docker.io/alpine:3.13';
   }
 };
 
@@ -61,7 +61,8 @@ const getDebugPod = async (name: string, namespace: string, nodeName: string): P
         {
           name: 'container-00',
           image,
-          command: ['/bin/sh'],
+          command: ['nsenter'],
+          args: ['-t', '1', '-m', '-u', '-i', '-n' 'sleep', '14000'],
           resources: {},
           volumeMounts: [
             {
